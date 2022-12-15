1aa4da05c3ffd1e044c6d218e5d49ad52fea80c4
Jony Hay <mejony@googlemail.com>
2022-12-15 12:24:17 +0000

    Day 15

--- 15_beacon_exclusion_zone/15_beacons.py
+++ 15_beacon_exclusion_zone/15_beacons.py
@@ -6,29 +6,6 @@ map_max_x = None
 map_min_y = None
 map_max_y = None
 
-# It's nice to be able to see things
-def print_map(out_of_range = set()):
-    if map_max_y - map_min_y > 100: # Do not print wider than console
-        return
-    for y in range(map_min_y,map_max_y):
-        for x in range(map_min_x - 5,map_max_x + 5):
-            drawn = False
-            for s in sensors:
-                if s[0] == x and s[1] == y:
-                    print("S", end='')
-                    drawn = True
-            for b in set(beacons):
-                if b[0] == x and b[1] == y:
-                    print("B", end='')
-                    drawn = True
-            if not drawn:
-                cell = x,y
-                if cell in out_of_range:
-                    print("#", end='')
-                else:
-                    print(".", end='')
-        print()
-
 # Also updates map bound variables
 def extract_coordinates(txt):
     global map_min_x, map_max_x, map_min_y, map_max_y
@@ -56,7 +33,6 @@ for x in [i.strip().split(":") for i in open('15_beacon_exclusion_zone/input.txt
 # Calculate range set
 in_range = set()
 for i, sensor in enumerate(sensors):
-    print(i, sensor)
     y = 2000000
     sensor_range = ranges[i]
     for x in range(sensor[0] - sensor_range, sensor[0] + sensor_range):
@@ -65,9 +41,43 @@ for i, sensor in enumerate(sensors):
             in_range.add(cell)
 
 # Remove beacons from out_of_range
-print("Removing beacon positions from in_range data ...")
+print("Removing beacon/sensor positions from in_range data ...")
 for b in set(beacons):
     in_range.discard(b)
+for s in sensors:
+    in_range.discard(s)
 
-print_map(in_range)
-print("Positions in range:", len(in_range))
+print("Positions in range:", len(in_range)) # 5688618
+
+##### PART 2
+print()
+print("===============================================================")
+print()
+
+def check_cell(cell):
+    #cells_checked.add(cell)
+    if 0 <= cell[0] <= search_bounds and 0 <= cell[1] <= search_bounds:
+        for i, s in enumerate(sensors):
+            sr = ranges[i]            
+            dist = distance(cell, s)
+            if dist <= sr:
+                return
+
+        print("Winning cell is", cell)
+        tun_freq = (cell[0] * 4000000) + cell[1]
+        print("Tuning Frequency", tun_freq)
+        exit(0)
+
+search_bounds = 4000000
+for i, sensor in enumerate(sensors):
+    sensor_range = ranges[i]
+    my_boundary = set()
+    rel_y = 1
+    for y in range(sensor[1] - sensor_range - 1, sensor[1] + sensor_range + 2):
+        check_cell( ((sensor[0] - rel_y + 1), y))
+        check_cell( ((sensor[0] + rel_y - 1), y))
+        if y < sensor[1]:
+            rel_y += 1
+        else:
+            rel_y -= 1
+    
